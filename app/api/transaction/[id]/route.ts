92072ccf21947ddb2b933e7e1eeb8cc91f572365
Christoph Weissteiner <30799919+theaussie86@users.noreply.github.com>
2024-05-07 19:55:10 +0200

    finalised api routes

--- app/api/transaction/[id]/route.ts
+++ app/api/transaction/[id]/route.ts
@@ -1,13 +1,58 @@
+import connectMongo from "@/libs/mongoose";
+import Transaction from "@/models/Transaction";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
-  return NextResponse.json({ message: "getting a transaction by id" });
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    await connectMongo();
+    const transaction = await Transaction.findById(params.id);
+
+    if (!transaction) {
+      return NextResponse.json({ error: "Transaction not found", status: 404 });
+    }
+    return NextResponse.json(transaction);
+  } catch (error) {
+    return NextResponse.json({ error: (error as Error).message, status: 500 });
+  }
 }
 
-export async function PUT(req: NextRequest) {
-  return NextResponse.json({ message: "updating a transaction by id" });
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    await connectMongo();
+    const transaction = await Transaction.findById(params.id);
+
+    if (!transaction) {
+      return NextResponse.json({ error: "Transaction not found", status: 404 });
+    }
+
+    transaction.set(req.body);
+    await transaction.save();
+
+    return NextResponse.json(transaction);
+  } catch (error) {
+    return NextResponse.json({ error: (error as Error).message, status: 500 });
+  }
 }
 
-export async function DELETE(req: NextRequest) {
-  return NextResponse.json({ message: "deleting a transaction by id" });
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    await connectMongo();
+    const transaction = await Transaction.findById(params.id);
+    if (!transaction) {
+      return NextResponse.json({ error: "Transaction not found", status: 404 });
+    }
+    const deletedTransaction = await transaction?.deleteOne();
+    return NextResponse.json({ deletedTransaction, status: 202 });
+  } catch (error) {
+    return NextResponse.json({ error: (error as Error).message, status: 500 });
+  }
 }
--- app/api/transaction/route.ts
+++ app/api/transaction/route.ts
@@ -1,5 +1,15 @@
+import connectMongo from "@/libs/mongoose";
+import Transaction from "@/models/Transaction";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  return NextResponse.json({ message: "creating a new transaction" });
+  try {
+    await connectMongo();
+    const transaction = new Transaction(req.body);
+    await transaction.save();
+
+    return NextResponse.json(transaction);
+  } catch (error) {
+    return NextResponse.json({ error: (error as Error).message, status: 500 });
+  }
 }
